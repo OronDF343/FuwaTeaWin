2970785680ce8e8484f0b896082bb4a7fd148c76
OronDF343 <orondf343@gmail.com>
2015-06-16 12:46:55 +0300

    License header

--- FuwaTea.Data/Properties/AssemblyInfo.cs
+++ FuwaTea.Data/Properties/AssemblyInfo.cs
@@ -1,4 +1,21 @@
-﻿using System.Reflection;
+﻿#region License
+//     This file is part of FuwaTeaWin.
+// 
+//     FuwaTeaWin is free software: you can redistribute it and/or modify
+//     it under the terms of the GNU General Public License as published by
+//     the Free Software Foundation, either version 3 of the License, or
+//     (at your option) any later version.
+// 
+//     FuwaTeaWin is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//     GNU General Public License for more details.
+// 
+//     You should have received a copy of the GNU General Public License
+//     along with FuwaTeaWin.  If not, see <http://www.gnu.org/licenses/>.
+#endregion
+
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
