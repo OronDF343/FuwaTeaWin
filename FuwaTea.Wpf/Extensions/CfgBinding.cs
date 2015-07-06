3908529b2f568b348408fd643b54c2ccbda0575e
OronDF343 <orondf343@gmail.com>
2015-07-06 17:22:06 +0300

    Settings Dynamic Bindings
    * CfgBinding is a binding to a specific setting. Key is constant.
    Nothing too special
    * CfgDynBinding is a binding to a setting with a key from a binding.
    SUPER HACKY - is a MultiBinding with a very special converter
    * Changed bindings in DataTemplates of settings DataGrid to
    CfgDynBinding, which makes them fire PropertyChanged when changed!

---
+++ FuwaTea.Wpf/Extensions/CfgBinding.cs
@@ -0,0 +1,80 @@
+﻿using System.ComponentModel;
+using System.Configuration;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Data;
+using FuwaTea.Annotations;
+
+namespace FuwaTea.Wpf.Extensions
+{
+    public class CfgBinding : Binding, INotifyPropertyChanged
+    {
+        public CfgBinding()
+        {
+            Path = new PropertyPath("Value");
+            Source = this;
+        }
+
+        public CfgBinding(string key) : this()
+        {
+            Key = key;
+        }
+
+        private string _key;
+        public string Key
+        {
+            get { return _key; }
+            set
+            {
+                if (_key == value) return;
+                _key = value;
+                UpdateNewValue();
+            }
+        }
+
+        private object _value;
+        public object Value
+        {
+            get { return _value; }
+            set
+            {
+                if (_value == value) return;
+                _value = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private ApplicationSettingsBase _repo;
+
+        public ApplicationSettingsBase Repository
+        {
+            get { return _repo; }
+            set
+            {
+                if (_repo != null) _repo.PropertyChanged -= RepoOnPropertyChanged;
+                _repo = value;
+                if (_repo != null) _repo.PropertyChanged += RepoOnPropertyChanged;
+                UpdateNewValue();
+            }
+        }
+
+        private void RepoOnPropertyChanged(object sender, PropertyChangedEventArgs args)
+        {
+            if (args.PropertyName == Key) UpdateNewValue();
+        }
+
+        private void UpdateNewValue()
+        {
+            Value = _repo == null ? null : Repository[Key];
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            var handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ FuwaTea.Wpf/Extensions/CfgDynBinding.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.ComponentModel;
+using System.Configuration;
+using System.Globalization;
+using System.Runtime.CompilerServices;
+using System.Windows.Data;
+using FuwaTea.Annotations;
+
+namespace FuwaTea.Wpf.Extensions
+{
+    public class CfgDynBinding : MultiBinding, INotifyPropertyChanged
+    {
+        public CfgDynBinding(BindingBase b)
+        {
+            Bindings.Add(b);
+            Bindings.Add(new Binding("Value") { Source = this });
+            Converter = new IndexerConverterProxy(this);
+        }
+
+        public object Value { get; set; }
+
+        private ApplicationSettingsBase _repo;
+
+        public ApplicationSettingsBase Repository
+        {
+            get { return _repo; }
+            set
+            {
+                if (_repo != null) _repo.PropertyChanged -= RepoOnPropertyChanged;
+                _repo = value;
+                if (_repo != null) _repo.PropertyChanged += RepoOnPropertyChanged;
+                UpdateNewValue();
+            }
+        }
+
+        private void RepoOnPropertyChanged(object sender, PropertyChangedEventArgs args)
+        {
+            if (((IndexerConverterProxy)Converter).Key == args.PropertyName) UpdateNewValue();
+        }
+
+        private void UpdateNewValue()
+        {
+            OnPropertyChanged("Value");
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            var handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    internal class IndexerConverterProxy : IMultiValueConverter
+    {
+        internal IndexerConverterProxy(CfgDynBinding cb)
+        {
+            Binding = cb;
+        }
+
+        internal CfgDynBinding Binding { get; private set; }
+
+        internal string Key { get; private set; }
+
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Binding.Repository[Key = (string)values[0]];
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            Binding.Repository[Key] = value;
+            return new[] { Key, Binding.Value };
+        }
+    }
+}
