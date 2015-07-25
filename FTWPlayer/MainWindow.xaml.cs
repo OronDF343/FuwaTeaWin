e2c752810a403a24903d93b9f867394f12e6c5e6
OronDF343 <orondf343@gmail.com>
2015-07-26 01:48:27 +0300

    Implemented new NotifyIcon
    * Now using Hardcodet.NotifyIcon.Wpf
    * Has all of the functionality that was missing previously
    * Removed dependency on System.Windows.Forms
    * New ContextMenu uses BLoc since it isn't part of the Logical Tree
    (which causes an error with Loc)

--- FTWPlayer/MainWindow.xaml.cs
+++ FTWPlayer/MainWindow.xaml.cs
@@ -20,18 +20,16 @@ using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Windows;
-using System.Windows.Forms;
 using System.Windows.Interop;
 using FTWPlayer.ViewModel;
 using FuwaTea.Lib;
-using Application = System.Windows.Application;
 
 namespace FTWPlayer
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : IDisposable
+    public partial class MainWindow
     {
         /// <summary>
         /// Initializes a new instance of the MainWindow class.
@@ -44,91 +42,6 @@ namespace FTWPlayer
             Application.Current.Resources.MergedDictionaries.Add(new ResourceDictionary { Source = new Uri("pack://application:,,,/Skins/GlacierSkin.xaml") });
         }
 
-        #region NotifyIcon
-
-        private NotifyIcon _controlNotifyIcon;
-        private void InitNotifyIcon()
-        {
-            // TODO: Clean up old code
-            _controlNotifyIcon = new NotifyIcon();
-            var iconContextMenuStrip = new ContextMenuStrip();
-            var playPauseToolStripMenuItem = new ToolStripMenuItem();
-            var previousToolStripMenuItem = new ToolStripMenuItem();
-            var nextToolStripMenuItem = new ToolStripMenuItem();
-            var stopToolStripMenuItem = new ToolStripMenuItem();
-            var toolStripSeparator1 = new ToolStripSeparator();
-            var showGuiToolStripMenuItem = new ToolStripMenuItem();
-            var exitToolStripMenuItem = new ToolStripMenuItem();
-
-            _controlNotifyIcon.BalloonTipIcon = ToolTipIcon.Info;
-            _controlNotifyIcon.BalloonTipTitle = "FTW Player";
-            _controlNotifyIcon.ContextMenuStrip = iconContextMenuStrip;
-            _controlNotifyIcon.Text = "FTW Player";
-            _controlNotifyIcon.Icon = System.Drawing.Icon.ExtractAssociatedIcon(Assembly.GetExecutingAssembly().Location); // TODO: this is temporary
-            _controlNotifyIcon.Visible = true;
-            _controlNotifyIcon.DoubleClick += (sender, e) => Show();
-
-            iconContextMenuStrip.Items.AddRange(new ToolStripItem[] {
-            playPauseToolStripMenuItem,
-            previousToolStripMenuItem,
-            nextToolStripMenuItem,
-            stopToolStripMenuItem,
-            toolStripSeparator1,
-            showGuiToolStripMenuItem,
-            exitToolStripMenuItem});
-            iconContextMenuStrip.Name = "iconContextMenuStrip";
-            iconContextMenuStrip.Size = new System.Drawing.Size(178, 164);
-            // 
-            // playPauseToolStripMenuItem
-            // 
-            playPauseToolStripMenuItem.Name = "playPauseToolStripMenuItem";
-            playPauseToolStripMenuItem.Size = new System.Drawing.Size(177, 22);
-            playPauseToolStripMenuItem.Text = "Play";
-            //playPauseToolStripMenuItem.Click += PlayButton_Click;
-            // 
-            // previousToolStripMenuItem
-            // 
-            previousToolStripMenuItem.Name = "previousToolStripMenuItem";
-            previousToolStripMenuItem.Size = new System.Drawing.Size(177, 22);
-            previousToolStripMenuItem.Text = "Previous";
-            //previousToolStripMenuItem.Click += PreviousButton_Click;
-            // 
-            // nextToolStripMenuItem
-            // 
-            nextToolStripMenuItem.Name = "nextToolStripMenuItem";
-            nextToolStripMenuItem.Size = new System.Drawing.Size(177, 22);
-            nextToolStripMenuItem.Text = "Next";
-            //nextToolStripMenuItem.Click += NextButton_Click;
-            // 
-            // stopToolStripMenuItem
-            // 
-            stopToolStripMenuItem.Name = "stopToolStripMenuItem";
-            stopToolStripMenuItem.Size = new System.Drawing.Size(177, 22);
-            stopToolStripMenuItem.Text = "Stop";
-            //stopToolStripMenuItem.Click += StopButton_Click;
-            // 
-            // toolStripSeparator1
-            // 
-            toolStripSeparator1.Name = "toolStripSeparator1";
-            toolStripSeparator1.Size = new System.Drawing.Size(174, 6);
-            // 
-            // showGuiToolStripMenuItem
-            // 
-            showGuiToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
-            showGuiToolStripMenuItem.Name = "showGuiToolStripMenuItem";
-            showGuiToolStripMenuItem.Size = new System.Drawing.Size(177, 22);
-            showGuiToolStripMenuItem.Text = "Show GUI";
-            //showMiniPlayerToolStripMenuItem.Click += ShowButton_Click;
-            // 
-            // exitToolStripMenuItem
-            // 
-            exitToolStripMenuItem.Name = "exitToolStripMenuItem";
-            exitToolStripMenuItem.Size = new System.Drawing.Size(177, 22);
-            exitToolStripMenuItem.Text = "Exit";
-            exitToolStripMenuItem.Click += (sender, args) => CloseButton_OnClick(sender, new RoutedEventArgs());
-        }
-        #endregion
-
         #region Single Instance Application
 
         private void MainWindow_OnSourceInitialized(object sender, EventArgs e)
@@ -136,8 +49,6 @@ namespace FTWPlayer
             // Single Instance Application hook
             var source = (HwndSource)PresentationSource.FromVisual(this);
             source.AddHook(HwndSourceHook); // should never be null
-            // Continue:
-            InitNotifyIcon();
         }
 
         private IntPtr HwndSourceHook(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
@@ -154,6 +65,11 @@ namespace FTWPlayer
         }
         #endregion
 
+        private void MaximizeButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            Show();
+        }
+
         private void MinimizeButton_OnClick(object sender, RoutedEventArgs e)
         {
             Hide();
@@ -163,25 +79,6 @@ namespace FTWPlayer
         private void CloseButton_OnClick(object sender, RoutedEventArgs e)
         {
             Close();
-        }
-
-        protected virtual void Dispose(bool disposing)
-        {
-            if (!disposing) return;
-            if (_controlNotifyIcon == null) return;
-            _controlNotifyIcon.Visible = false;
-            _controlNotifyIcon.Dispose();
-        }
-
-        public void Dispose()
-        {
-            Dispose(true);
-            GC.SuppressFinalize(this);
-        }
-
-        private void MainWindow_OnClosed(object sender, EventArgs e)
-        {
-            Dispose();
         }
     }
 }
