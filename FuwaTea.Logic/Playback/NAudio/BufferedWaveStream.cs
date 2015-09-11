7d1ae99b080f0906bab9c6db4addd192f337df99
OronDF343 <orondf343@gmail.com>
2015-09-11 17:02:03 +0300

    Buffer tweaks 2

--- FuwaTea.Logic/Playback/NAudio/BufferedWaveStream.cs
+++ FuwaTea.Logic/Playback/NAudio/BufferedWaveStream.cs
@@ -20,8 +20,8 @@ namespace FuwaTea.Logic.Playback.NAudio
 
         public override int Read(byte[] buffer, int offset, int count)
         {
-            // If buffer is at less than 20%, fill it
-            if (!_filling && _bwp.BufferedBytes < _bwp.BufferLength / 5)
+            // If buffer is at less than RefillAtPercent, fill it
+            if (!_filling && _bwp.BufferedBytes < _bwp.BufferLength / 100 * RefillAtPercent)
                 (_th = new Thread(FillBuffer)).Start();
             return _bwp.Read(buffer, offset, count);
         }
@@ -30,13 +30,13 @@ namespace FuwaTea.Logic.Playback.NAudio
         private void FillBuffer()
         {
             _filling = true;
-            // Fill buffer to 100%
+            // Try to fill the buffer completely
             var bytesToFill = _bwp.BufferLength - _bwp.BufferedBytes;
-            // Do it in 4 batches
-            for (var i = 0; i < 4; i++)
+            // Do it in RefillBatches
+            for (var i = 0; i < RefillBatches; i++)
             {
-                var buffer = new byte[bytesToFill / 4];
-                var read = _src.Read(buffer, 0, bytesToFill / 4);
+                var buffer = new byte[bytesToFill / RefillBatches];
+                var read = _src.Read(buffer, 0, bytesToFill / RefillBatches);
                 _bwp.AddSamples(buffer, 0, read);
             }
             _filling = false;
@@ -44,7 +44,9 @@ namespace FuwaTea.Logic.Playback.NAudio
 
         public override WaveFormat WaveFormat => _src.WaveFormat;
 
-        public TimeSpan BufferSize { get { return _bwp.BufferDuration; } set { _bwp.BufferDuration = value; } }
+        public TimeSpan BufferDuration { get { return _bwp.BufferDuration; } set { _bwp.BufferDuration = value; } }
+        public int RefillAtPercent { get; set; } = 33;
+        public int RefillBatches { get; set; } = 3;
 
         public override long Length => (long)(_bwp.BufferDuration.TotalSeconds * WaveFormat.AverageBytesPerSecond);
         public override long Position { get { return (long)(_bwp.BufferedDuration.TotalSeconds * WaveFormat.AverageBytesPerSecond); } set { throw new NotSupportedException(); } }
