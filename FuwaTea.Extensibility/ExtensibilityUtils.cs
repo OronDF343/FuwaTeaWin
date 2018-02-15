9848482f8102f67376d41c34cac4654d9e5029a6
OronDF343 <orondf343@gmail.com>
2018-02-15 18:07:33 +0200

    Added "scoped" import/export attributes

---
+++ FuwaTea.Extensibility/ExtensibilityUtils.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FuwaTea.Extensibility
+{
+    public static class ExtensibilityUtils
+    {
+        public static string MakeScopedKey(string module, string area, string name)
+        {
+            return module + "." + area + "." + name;
+        }
+    }
+}
---
+++ FuwaTea.Extensibility/ScopedImportAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.Composition;
+
+namespace FuwaTea.Extensibility
+{
+    public class ScopedImportAttribute : ImportAttribute
+    {
+        public ScopedImportAttribute(string module, string area, string name)
+            : base(ExtensibilityUtils.MakeScopedKey(module, area, name))
+        {
+            Module = module;
+            Area = area;
+            Name = name;
+        }
+
+        public string Name { get; }
+
+        public string Area { get; }
+
+        public string Module { get; }
+    }
+}
