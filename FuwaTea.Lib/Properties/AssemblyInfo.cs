d48c30ff64f9e46f82d82554688a97a89e8dc4a5
OronDF343 <orondf343@gmail.com>
2024-02-24 21:10:39 +0200

    Prep: Multi-targeting libs

--- FuwaTea.Lib/Properties/AssemblyInfo.cs
+++ FuwaTea.Lib/Properties/AssemblyInfo.cs
@@ -15,42 +15,7 @@
 //     along with FuwaTeaWin.  If not, see <http://www.gnu.org/licenses/>.
 #endregion
 
-using System.Reflection;
-using System.Runtime.InteropServices;
 using FuwaTea.Lib;
 using ModularFramework.Attributes;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("FuwaTea.Lib")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("OronDF343")]
-[assembly: AssemblyProduct("FuwaTea.Lib")]
-[assembly: AssemblyCopyright("Copyright © 2015 OronDF343")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("91d28c20-e2ef-44ae-a1f8-c0e0e940b4e8")]
-
 [assembly: ModuleDefinition("LibComponents", typeof(LibComponentAttribute), typeof(ILibComponent))]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
